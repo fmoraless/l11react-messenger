047dfa7cb3224e8ce122861d5d0b3d158bb46faf
fmoraless <franticzero@gmail.com>
2024-05-08 13:51:55 -0400

    disabling echo js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,9 +1,9 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
-window.Echo = new Echo({
+/* window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
@@ -12,3 +12,4 @@ window.Echo = new Echo({
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
+ */
